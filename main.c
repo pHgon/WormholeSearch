3eeaef9a44348faedfe6782cdbd76d91a5d40a23
Gilberto Kreisler <gkreisler5@hotmail.com>
2017-03-10 22:32:11 -0300

    Funções ajustadas, tudo funcionando

--- main.c
+++ main.c
@@ -11,8 +11,9 @@ struct Node{
 	struct Node *pred; // Predecessor
 }; typedef struct Node Wormhole;
 
-void wormholeSearch (FILE *inputFile, int numGalaxies, int numWormholes);
-//void BellmanFord (Wormhole *matrix, int numGalaxies, int numWormholes, int src);
+void wormholeSearch (FILE *inputFile, FILE *outputFile, int numGalaxies, int numWormholes);
+int BellmanFord (Wormhole *list, int numGalaxies, int numWormholes, int matrix[][numWormholes]);
+void relax(Wormhole *u, Wormhole *v, int w);
 
 void main (int argc,char *argv[]){
 	if(argc == 1){
@@ -28,23 +29,27 @@ void main (int argc,char *argv[]){
 
 	int i, sets;
 	fscanf(inputFile, "%d\n", &sets);  // Le o numero de conjuntos que serao avaliados
+	FILE *outputFile = fopen("log_output.txt", "w");
 
 	for(i=0; i<sets; i++){  // Para todos os conjuntos
 		int galaxies, wormholes;
 		fscanf(inputFile, "%d %d\n", &galaxies, &wormholes);  // Le o numero de galaxias e o numero de buracos de minhoca
-		wormholeSearch(inputFile, galaxies, wormholes);
+		wormholeSearch(inputFile, outputFile, galaxies, wormholes);
 	}
+
+	fclose(inputFile);
+	fclose(outputFile);
 }
 
-void wormholeSearch (FILE *inputFile, int numGalaxies, int numWormholes){
+void wormholeSearch (FILE *inputFile, FILE *outputFile, int numGalaxies, int numWormholes){
 	int matrix[numGalaxies][numWormholes]; //Matriz de adjacência
-	int vertices[numGalaxies]; //Lista de vertices do grafo
-	int i, j, src = INT_MAX;
+	Wormhole vertices[numGalaxies]; //Lista de vertices do grafo
+	int i, j, src = INT_MAX, possible;
 	int aux1, aux2, aux3;  // Auxiliares para leitura do arquivo
 
 	for(i=0; i<numGalaxies; i++){         //
-		for(j=0; j<numWormholes; j++){    //  NAO SEI AO CERTO SE ESTE TRECHO E NECESSARIO
-			matrix[i][j] = 0;        //  A MATRIZ JA INICIA ZERADA?
+		for(j=0; j<numWormholes; j++){    //  Matriz tbm iniciada com INT_MAX
+			matrix[i][j] = INT_MAX;        //  para identificar as posições não usadas
 		}                                 //
 	}                                     //
 
@@ -52,58 +57,55 @@ void wormholeSearch (FILE *inputFile, int numGalaxies, int numWormholes){
 	for(i=0; i<numWormholes; i++){
 		fscanf(inputFile, "%d %d %d\n", &aux1, &aux2, &aux3);
 		matrix[aux1][aux2] = aux3;
-		if(aux1<src)
-			src=aux1;
+		if(aux1 < src)
+			src = aux1;
 	}
-
-	// BELLMAN FORD ALGORITHM //////////////////////////////////////////////////////////////////////
 
 	//Starta a distancia de cada vertice com infinito e predecessor nulo
 	for(i = 0; i < numGalaxies; i++){
-		vertices[i].dist = INT_MAX; // Seta todos os vertices com infinito
-		vertices[i]->pred = NULL;
+		vertices[i].distance = INT_MAX; // Seta todos os vertices com infinito
+		vertices[i].pred = NULL;
 	}
-	vertices[src].dist = 0; // Vertice inicial recebe 0
+	vertices[src].distance = 0; // Vertice inicial recebe 0
 
-	BellmanFord(vertices, matrix, numGalaxies, numWormholes,src);
+	possible = BellmanFord(vertices, numGalaxies, numWormholes, matrix);
 
+	if(possible){
+		fprintf(outputFile, "possivel\n");
+	}
+	else{
+		fprintf(outputFile, "impossivel\n");
+	}
 
 }
 
-void BellmanFord (Wormhole *list, int matrix[][numWormholes], int numGalaxies, int numWormholes, int src){
-	int i,j;
+int BellmanFord (Wormhole *list, int numGalaxies, int numWormholes, int matrix[][numWormholes]){
+	int i, j, k;
 
-	for(i=0; i<numGalaxies; i++){
-		for(j=0; j<numWormholes; j++){
-			printf("%5d ", matrix[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-
-	//Acho que aqui deveria ser um for triplo
 	//Pra cada vertice - vertice(inicial) -> percorre cada aresta
-	for (i = 0; i < numGalaxies; i++) {
-		for (j = 0; j < numWormholes; j++) {
-			if (matrix[i][j] != 0)
-				relax(list[i], list[j], matrix[i][j]);
+	for(k = 0; k < numGalaxies - 1; k++){
+		for (i = 0; i < numGalaxies; i++) {
+			for (j = 0; j < numWormholes; j++) {
+				if(matrix[i][j] != INT_MAX)
+					relax(&list[i], &list[j], matrix[i][j]);
+			}
 		}
 	}
 
 	for (i = 0; i < numGalaxies; i++) {
 		for (i = 0; i < numWormholes; i++) {
-			if (matrix[i][j] != 0 && list[i].dist + matrix[i][j] < list[j].dist) {
-				//CICLO NEGATIVO - POSSIVEL
+			if (matrix[i][j] != INT_MAX && list[i].distance + matrix[i][j] < list[j].distance) {
+				return 0;
 			}
 		}
 	}
 
-	//IMPOSSIVEL
+	return 1;
 }
 
-void relax(Wormhole u, Wormhole v, int w){
-	if (u.dist + w < v.dist) {
-		v.dist = u.dist + w;
+void relax(Wormhole *u, Wormhole *v, int w){
+	if (u->distance + w < v->distance) {
+		v->distance = u->distance + w;
 		v->pred = u;
 	}
 
